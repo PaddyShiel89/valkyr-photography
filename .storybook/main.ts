044d758e86e8c1ee3e5689eb5bc0af95369c83f5
Patrick Shiel <p.shiel@live.co.uk>
2023-09-01 12:36:49 +0100

    chore: Storybook accessibility and testing addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,10 +4,10 @@ import * as path from "path";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
+    "@storybook/addon-a11y",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-links",
   ],
   framework: {
     name: "@storybook/react-webpack5",
