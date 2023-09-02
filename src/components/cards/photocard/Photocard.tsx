24c2999649a20abdbbbacb46196933e2e386aac1
Patrick Shiel <p.shiel@live.co.uk>
2023-09-02 16:50:54 +0100

    feat: Photocard component layout

--- src/components/cards/photocard/Photocard.tsx
+++ src/components/cards/photocard/Photocard.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { base as clBase } from "./Photocard.module.scss";
+import { base as pcBase } from "./Photocard.module.scss";
 
 const Photocard = () => {
-  return <div className={clBase}>Photocard</div>;
+  return (
+    <div className={pcBase}>
+      <img
+        src="https://unsplash.it/600/400"
+        alt="Temporary image for development purposes."
+      />
+      <div>Categories</div>
+      <h5>
+        <div>Heading</div>
+        <div>Subheading</div>
+      </h5>
+      <div>Body content</div>
+      <div>Link</div>
+    </div>
+  );
 };
 
 export default Photocard;
