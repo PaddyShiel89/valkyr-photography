e290bc461c7a1322799af6f6a8673b3300b8eaa7
Patrick Shiel <p.shiel@live.co.uk>
2023-09-02 16:43:09 +0100

    feat: Base photocard files

---
+++ src/components/cards/photocard/Photocard.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Photocard from "./Photocard";
+
+const meta: Meta<typeof Photocard> = {
+  title: "Cards/Photocard",
+  component: Photocard,
+};
+
+export default meta;
+type Story = StoryObj<typeof Photocard>;
+
+export const Default: Story = {};
---
+++ src/components/cards/photocard/Photocard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { base as clBase } from "./Photocard.module.scss";
+
+const Photocard = () => {
+  return <div className={clBase}>Photocard</div>;
+};
+
+export default Photocard;
