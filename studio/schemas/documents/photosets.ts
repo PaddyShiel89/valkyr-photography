48f53edb84d55216cab42450e5b33692de267da4
Patrick Shiel <p.shiel@live.co.uk>
2023-09-02 17:37:49 +0100

    feat: Initial photosets schema

---
+++ studio/schemas/documents/photosets.ts
@@ -0,0 +1,35 @@
+import {SchemaTypeDefinition} from 'sanity'
+
+const photosets: SchemaTypeDefinition = {
+  title: 'Photosets',
+  name: 'photosets',
+  type: 'document',
+  fields: [
+    {
+      name: 'models',
+      type: 'string',
+      title: 'Models',
+      description: 'The models pictured in the photoset. Also used as the title of the photoset.',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'date',
+      type: 'date',
+      title: 'Date',
+      description:
+        'The date the photoset was shot. The month and year is used as the subtitle of the photoset.',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'description',
+      type: 'array',
+      of: [{type: 'block'}],
+      title: 'Description',
+      description:
+        'A brief description of the photoset, no more than a couple of short paragraphs.',
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+}
+
+export default photosets
--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import photosets from './documents/photosets'
+
+export const schemaTypes = [photosets]
