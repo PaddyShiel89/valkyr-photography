18d460f1e186dd935092cee50bb52ee5cc9fa2db
Patrick Shiel <p.shiel@live.co.uk>
2023-09-10 00:08:12 +0100

    chore: sanityPhotosets dummy data

--- testing/data/index.ts
+++ testing/data/index.ts
@@ -1,2 +1,3 @@
 export { default as allSanityPhotoCategories } from "./allSanityPhotoCategories";
 export { default as photosetCardQuery } from "./photosetCardQuery";
+export { default as sanityPhotosets } from "./sanityPhotosets";
