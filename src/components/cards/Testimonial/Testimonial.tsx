9c4e0ec1ceacce5331f8214d275f69738d7d93e6
Patrick Shiel <p.shiel@live.co.uk>
2023-09-13 21:24:46 +0100

    feat: Testimonial image styling

--- src/components/cards/Testimonial/Testimonial.tsx
+++ src/components/cards/Testimonial/Testimonial.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { GatsbyImage, IGatsbyImageData, getImage } from "gatsby-plugin-image";
 
 import {
   base as cBase,
   body as cBody,
   date as cDate,
   footer as cFooter,
+  image as cImage,
   name as cName,
 } from "./Testimonial.module.scss";
 import { getMonthFromSanityDate, getYearFromSanityDate } from "@helpers";
 
-const Testimonial = ({ date, ...props }: TestimonialProps) => {
+const Testimonial = ({ date, image, ...props }: TestimonialProps) => {
+  /* ------------------------------- Format date ------------------------------ */
+
   const month = getMonthFromSanityDate(date || "");
   const year = getYearFromSanityDate(date || "");
 
+  /* -------------------------- GatsbyImage component ------------------------- */
+
+  const imgData = getImage(image.gatsbyImage) as IGatsbyImageData;
+
   return (
     <div className={cBase}>
-      <div>Img</div>
+      <div className={cImage}>
+        <GatsbyImage alt={image.altText || ""} image={imgData} />
+      </div>
       <ReactMarkdown className={cBody} children={props.quote || ""} />
       <div className={cFooter}>
         <div className={cName}>
